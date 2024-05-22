2f9405516e703015398ebe5500f312e3116dcb19
ADIOP55550 <klamutadrian382@gmail.com>
2024-05-22 02:43:36 +0200

    Code reading from file

--- tumasim.c
+++ tumasim.c
@@ -2,9 +2,11 @@
 #include <assert.h>  // assert
 #include <stdbool.h> // bool, true, false
 #include <stddef.h>
-#include <stdio.h>  // printf
-#include <stdlib.h> // malloc, free
-#include <string.h> // memcpy, strlen
+#include <stdio.h>  // printf, fseek, fopen, fclose, ftell, fread
+#include <stdlib.h> // malloc, free, perror,
+#include <string.h> // memcpy, strlen, strdup, strtok, strncmp, strchr
+
+bool debug = false;
 
 typedef enum { STOP, LEFT, RIGHT } PointerMove;
 
@@ -22,7 +24,7 @@ struct Rule {
 struct TMState {
   char name;
   size_t rules_count;
-  struct Rule rules[];
+  struct Rule *rules;
 };
 
 // Turing Machine
@@ -45,47 +47,135 @@ typedef struct Rule Rule;
 typedef struct TM TM;
 typedef struct RunningTM RunningTM;
 
-TMState A, B, C, D, E, F, G;
+void print_rule(const Rule *rule) {
+  if (rule == NULL) {
+    printf("Rule is NULL\n");
+    return;
+  }
 
-TMState A = {'A',
-             1,
-             {
-                 (Rule){'1', '1', &B, R},
-             }};
+  printf("Rule:\n");
+  printf("  In Value: '%c' (%i)\n", rule->in_value, rule->in_value);
+  printf("  Out Value: '%c' (%i)\n", rule->out_value, rule->out_value);
+  printf("  Target State: %c\n",
+         rule->target_state ? rule->target_state->name : ' ');
+  printf("  Direction: %s\n", rule->direction == L
+                                  ? "LEFT"
+                                  : (rule->direction == R ? "RIGHT" : "STOP"));
+}
+char *read_file(const char *filename) {
+  FILE *file = fopen(filename, "r");
+  if (!file) {
+    perror("Could not open file");
+    exit(EXIT_FAILURE);
+  }
 
-TMState B = {'B',
-             2,
-             {
-                 (Rule){'1', '1', &C, R},
-                 (Rule){'0', '0', &D, R},
-             }};
+  fseek(file, 0, SEEK_END);
+  long length = ftell(file);
+  fseek(file, 0, SEEK_SET);
 
-TMState C = {'C',
-             2,
-             {
-                 (Rule){'0', '0', &D, R},
-                 (Rule){'1', '1', &G, R},
-             }};
+  char *buffer = malloc(length + 1);
+  if (!buffer) {
+    perror("Could not allocate memory");
+    exit(EXIT_FAILURE);
+  }
 
-TMState D = {'D', 2, {(Rule){'0', '0', &D, R}, (Rule){'1', '1', &E, R}}};
+  fread(buffer, 1, length, file);
+  buffer[length] = '\0';
 
-TMState E = {'E', 1, {(Rule){0, 0, &F, S}}};
+  fclose(file);
+  return buffer;
+}
 
-TMState F = {'F', 0, {}};
+TMState *find_state(TMState **states, char name) {
+  for (int i = 0; states[i] != NULL; i++) {
+    if (states[i]->name == name) {
+      return states[i];
+    }
+  }
+  return NULL;
+}
 
-TMState G = {
-    'G',
-    3,
-    {(Rule){'0', '0', &D, R}, (Rule){'1', '1', &G, R}, (Rule){0, 0, &F, S}}};
+void parse_input(const char *input, TM *machine) {
+  char *input_copy = strdup(input);
+  char *saveptr1, *saveptr2;
+  char *line = strtok_r(input_copy, "\n", &saveptr1);
 
-TMState *all_states[] = {&A, &B, &C, &D, &E, &F, &G, NULL};
+  TMState **all_states = malloc(100 * sizeof(TMState *));
+  int state_count = 0;
 
-TMState *accepting_states[] = {&C, &F, NULL};
+  if (strncmp(line, "DEBUG ", 6) == 0) {
+    debug = line[6] == '1';
+    line = strtok_r(NULL, "\n", &saveptr1); // Move to the next line
+  }
 
-TM machine = {&A, all_states, accepting_states};
+  // Read all_states from the first line
+  char *state_token = strtok_r(line, ", ", &saveptr2);
+  while (state_token) {
+    TMState *state = malloc(sizeof(TMState));
+    state->name = state_token[0];
+    state->rules_count = 0;
+    state->rules = malloc(10 * sizeof(Rule));
+    all_states[state_count++] = state;
+    if (debug) {
+      printf("Found state [%c]\n", state->name);
+    }
+    state_token = strtok_r(NULL, ", ", &saveptr2);
+  }
+  all_states[state_count] = NULL;
 
+  line = strtok_r(NULL, "\n", &saveptr1); // Move to the next line
 
+  TMState **accepting_states = malloc(100 * sizeof(TMState *));
+  int accepting_count = 0;
+  char initial_state = 0;
 
+  while (line) {
+    if (debug) {
+      printf("line: [%s]\n", line);
+    }
+    if (strncmp(line, "init", 4) == 0) {
+      initial_state = line[5];
+    } else if (strncmp(line, "accept", 6) == 0) {
+      char *tok = strtok_r(line + 7, ", ", &saveptr2);
+      while (tok) {
+        accepting_states[accepting_count++] = find_state(all_states, tok[0]);
+        tok = strtok_r(NULL, ", ", &saveptr2);
+      }
+    } else if (strchr(line, ':')) {
+      char state_name = line[0];
+      TMState *state = find_state(all_states, state_name);
+      char *rule_line = strtok_r(NULL, "\n", &saveptr1);
+      while (rule_line && rule_line[0] == '\t') {
+        if (debug) {
+          printf("rule_line: [%s]\n", rule_line);
+        }
+        Rule rule;
+        rule.in_value = rule_line[1];
+        rule.out_value = rule_line[4];
+        rule.direction =
+            rule_line[7] == '>' ? R : (rule_line[7] == '<' ? L : S);
+        rule.target_state = find_state(all_states, rule_line[10]);
+
+        rule.in_value = rule.in_value == '_' ? 0 : rule.in_value;
+        rule.out_value = rule.out_value == '_' ? 0 : rule.out_value;
+
+        state->rules[state->rules_count++] = rule;
+        rule_line = strtok_r(NULL, "\n", &saveptr1);
+        if (debug) {
+          printf("New rule in state %c: \n", state->name);
+          print_rule(&rule);
+        }
+      }
+    }
+    line = strtok_r(NULL, "\n", &saveptr1);
+  }
+
+  machine->initial_state = find_state(all_states, initial_state);
+  machine->all_states = all_states;
+  machine->accepting_states = accepting_states;
+
+  free(input_copy);
+}
 
 bool is_accepting_state(const TM *const machine, const TMState *const state) {
   TMState *checked_state;
@@ -147,6 +237,10 @@ void print_tape(const RunningTM *const machine) {
     putchar(' ');
   }
   putchar('^');
+}
+
+void print_state(const RunningTM *const machine) {
+  printf("\nCurrent state: [%c]\n", machine->current_state->name);
 }
 
 char get_current_value(const RunningTM *const machine) {
@@ -218,6 +312,7 @@ TMState simulate_machine(const TM *const machine, const char *input,
 
   PointerMove next_move;
   do {
+    print_state(&running_machine);
     print_tape(&running_machine);
     getchar();
     next_move = step_machine(&running_machine);
@@ -241,15 +336,41 @@ bool check_accept(TM const *machine, const char *input,
 
 int main(const int argc, const char **argv) {
   // TM should accept 11*0*1
-
-  if (argc < 2) {
-    printf("Please provide input string.\n");
-    exit(1);
+  if (argc != 3) {
+    fprintf(stderr, "Usage: %s <code_file> <machine_input>\n", argv[0]);
+    return EXIT_FAILURE;
   }
 
-  print_string_as_int_codes(argv[1]);
+  char *code = read_file(argv[1]);
 
-  bool accepted = check_accept(&machine, argv[1], strlen(argv[1]));
+  TM machine;
+  parse_input(code, &machine);
 
+  if (debug) {
+    printf("Initial state: %c\n", machine.initial_state->name);
+    printf("Accepting states: ");
+    for (int i = 0; machine.accepting_states[i] != NULL; i++) {
+      printf("%c ", machine.accepting_states[i]->name);
+    }
+    printf("\n");
+
+    print_string_as_int_codes(argv[2]);
+  }
+
+  bool accepted = check_accept(&machine, argv[2], strlen(argv[2]));
+
+  putchar('\n');
   printf(accepted ? "ACCEPTED" : "REJECTED");
+  putchar('\n');
+
+  // Cleanup
+  for (int i = 0; machine.all_states[i] != NULL; i++) {
+    free(machine.all_states[i]->rules);
+    free(machine.all_states[i]);
+  }
+  free(machine.all_states);
+  free(machine.accepting_states);
+  free(code);
+
+  return 0;
 }
