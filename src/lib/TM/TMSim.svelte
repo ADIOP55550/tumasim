696a6598facd485186d1fce2eac6d83517235c43
ADIOP55550 <klamutadrian382@gmail.com>
2024-05-28 18:32:47 +0200

    Tape_width_calculation v1

--- src/lib/TM/TMSim.svelte
+++ src/lib/TM/TMSim.svelte
@@ -1,96 +1,214 @@
-
 <script>
-  let pointerPos = $state(5);
-  let pointerElem;
+	import { elasticOut, bounceOut } from 'svelte/easing';
+	import { flip } from 'svelte/animate';
+	import { tweened } from 'svelte/motion';
+	import { browser } from '$app/environment';
 
-  $effect(()=>{
-    console.log(pointerPos);
+	let cell_width = $state(38);
+	let window_inner_width = $state(1920);
+	let cells_shown = $state(18);
+	let pointerPos = $state(400);
+	let tape = $state(new Array(10000).fill(1).map((_, i) => ({ id: i, value: 0 })));
+	let cells = $derived(tape.slice(pointerPos - cells_shown / 2 + 1, cells_shown / 2 + pointerPos));
+	let ypos = tweened(0, { duration: 40, easing: bounceOut });
 
-    pointerElem.scrollIntoView()
-  });
-  
+	if (browser) {
+		window.addEventListener('resize', () => {
+			window_inner_width = window.innerWidth;
+		});
+		$effect(() => {
+			cells_shown = Math.floor(window_inner_width / cell_width);
+		});
+	}
 </script>
 
-<div class="button" on:click={()=>{pointerPos--}}>-</div>
-<div class="button" on:click={()=>{pointerPos++}}>+</div>
-<div id="tape">
-  <div class="cell" data-pos="0">_</div>
-  <div class="cell" data-pos="1">_</div>
-  <div class="cell" data-pos="2">_</div>
-  <div class="cell" data-pos="3">_</div>
-  <div class="cell" data-pos="4">_</div>
-  <div class="cell" data-pos="5">_</div>
-  <div class="cell" data-pos="6">_</div>
-  <div class="cell" data-pos="7">_</div>
-  <div class="cell" data-pos="8">_</div>
-  <div class="cell" data-pos="9">_</div>
-  <div class="cell" data-pos="10">_</div>
-  <div class="cell" data-pos="11">_</div>
-  <div class="cell" data-pos="12">_</div>
-  <div class="cell" data-pos="13">_</div>
-  <div class="cell" data-pos="14">_</div>
-  <div class="cell" data-pos="15">_</div>
-  <div class="cell" data-pos="16">_</div>
-  <div class="cell" data-pos="17">_</div>
-  <div class="cell" data-pos="18">_</div>
-  <div class="cell" data-pos="19">_</div>
-  <div class="cell" data-pos="20">_</div>
-  <div class="cell" data-pos="21">_</div>
-  <div class="cell" data-pos="22">_</div>
-  <div class="cell" data-pos="23">_</div>
-  <div class="cell" data-pos="24">_</div>
-  <div class="cell" data-pos="25">_</div>
-  <div class="cell" data-pos="26">_</div>
-  <div class="cell" data-pos="27">_</div>
-  <div class="cell" data-pos="28">_</div>
-  <div class="cell" data-pos="29">_</div>
-  <div class="cell" data-pos="30">_</div>
-  <div class="cell" data-pos="31">_</div>
-  <div class="cell" data-pos="32">_</div>
-  <div class="cell" data-pos="33">_</div>
-  <div class="cell" data-pos="34">_</div>
-  <div class="cell" data-pos="35">_</div>
-  <div class="cell" data-pos="36">_</div>
-  <div class="cell" data-pos="37">_</div>
-  <div class="cell" data-pos="38">_</div>
-  <div class="cell" data-pos="39">_</div>
-  <div class="cell" data-pos="40">_</div>
-  <div class="cell" data-pos="41">_</div>
-  <div class="cell" data-pos="42">_</div>
-  <div class="cell" data-pos="43">_</div>
-  <div class="cell" data-pos="44">_</div>
-  <div class="pointer-wrapper">
-    <div id="pointer" bind:this={pointerElem} style:grid-column={pointerPos + ' / ' + (pointerPos + 1)}></div>
-  </div>
+<input type="number" bind:value={cell_width} />
+{cells_shown}<br />
+<div
+	class="button"
+	on:click={() => {
+		pointerPos--;
+	}}
+>
+	&lt;
+</div>
+<div
+	class="button"
+	on:click={() => {
+		pointerPos++;
+	}}
+>
+	&gt;
+</div>
+<div
+	class="button"
+	on:click={() => {
+		tape[pointerPos].value += 1;
+		tape = tape;
+		ypos.set(-7).then(() => ypos.set(0));
+	}}
+>
+	+
+</div>
+<div
+	class="button"
+	on:click={() => {
+		tape[pointerPos].value -= 1;
+		tape = tape;
+		ypos.set(7).then(() => ypos.set(0));
+	}}
+>
+	-
+</div>
+<div id="tape-wrapper">
+	<div
+		id="tape"
+		on:wheel={(e) => {
+			e.preventDefault();
+			return false;
+		}}
+	>
+		{#each cells as cell, i (cell.id)}
+			<div
+				class="cell"
+				animate:flip={{ duration: 200 }}
+				style:transform={cell.id == pointerPos ? `translateY(${$ypos}px)` : ''}
+				data-pos={i}
+				on:click={() => (pointerPos = cell.id)}
+			>
+				{cell.value}
+			</div>
+		{/each}
+	</div>
+	<div id="pointer" class="gradient-border"></div>
 </div>
 
-
 <style>
-  #tape {
-    display: grid;
-    grid-template-columns: repeat(45, 1fr);
-    grid-template-rows: 1fr 1fr;
-    width: 100%;
-    gap: .1em;
-    overflow-x: scroll;
-  }
-  .cell {
-    border: 2px solid black;
-    width: 2em;
-    height: 2em;
-    vertical-align: middle;
-    text-align: center;
-  }
-  .pointer-wrapper {
-    display: grid;
-    grid-template-columns: subgrid;
-    grid-row: 2 / 3;
-    grid-column: 1 / -1;
-  }
-  #pointer {
-    display: block;
-    border-top: 5px solid black;
-    border-right: 5px solid black;
-    transform: rotateZ(-45deg) scale(0.3);
-  }
+	#tape-wrapper {
+		width: calc(100vw - 4em);
+		margin: auto;
+		position: relative;
+	}
+	#tape {
+		display: flex;
+		flex-flow: row nowrap;
+		width: max-content;
+		gap: 4px;
+		overflow-x: hidden;
+		width: 100%;
+	}
+	#tape-wrapper::before {
+		content: '';
+		display: block;
+		position: absolute;
+		left: 0;
+		top: 0;
+		background: rgb(255, 255, 255);
+		background: linear-gradient(
+			90deg,
+			rgba(255, 255, 255, 1) 0%,
+			rgba(255, 255, 255, 1) 35%,
+			rgba(255, 255, 255, 0) 100%
+		);
+		width: 6em;
+		z-index: 200;
+		height: 100%;
+	}
+	#tape-wrapper::after {
+		content: '';
+		display: block;
+		position: absolute;
+		right: 0;
+		top: 0;
+		background: rgb(255, 255, 255);
+		background: linear-gradient(
+			-90deg,
+			rgba(255, 255, 255, 1) 0%,
+			rgba(255, 255, 255, 1) 35%,
+			rgba(255, 255, 255, 0) 100%
+		);
+		width: 6em;
+		z-index: 200;
+		height: 100%;
+	}
+	.cell {
+		width: 1.9em;
+		height: 2em;
+		background: wheat;
+		border-radius: 0.3em;
+		text-align: center;
+		flex-shrink: 0;
+		color: black;
+		display: grid;
+		place-content: center;
+		margin-top: 1em;
+		margin-bottom: 1em;
+	}
+
+	#pointer {
+		width: 2em;
+		height: 4em;
+		position: absolute;
+		left: 50%;
+		top: -0.4em;
+		background: #bbbbbb33;
+		box-sizing: content-box;
+		z-index: 10;
+		transform: translate(-50%, 1em) translate(-0.4em);
+		padding: 0.1em;
+		border-radius: 3px;
+	}
+
+	#pointer::after {
+		content: '';
+		display: block;
+		position: absolute;
+		bottom: -0.7em;
+		width: 2em;
+		height: 2em;
+		border-top: 6px solid #7f27ab;
+		border-left: 6px solid #7f27ab;
+		border-radius: 0.1em;
+		transform: translate(-2px) rotateZ(45deg) scale(0.4);
+	}
+	.button {
+		user-select: none;
+	}
+
+	.gradient-border {
+		--box-border--border: linear-gradient(105deg, rgb(255 46 144) 0%, rgb(61 35 185) 100%);
+
+		/* classic 9-slide-scaling with 2px border 4px rounded corners. change `rx` and `ry` parameters to adjust border-radius */
+		--box--border__top-left: url("data:image/svg+xml,<svg width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'><rect x='1' y='1' width='18' height='18' rx='6' ry='6' stroke='%23000' stroke-width='3' /></svg>");
+		--box--border__top: url("data:image/svg+xml,<svg preserveAspectRatio='none' width='100' height='10' viewBox='0 0 100 10' fill='none' xmlns='http://www.w3.org/2000/svg'><line x1='-1' y1='1' x2='101' y2='1' stroke='%23000' stroke-width='3'/></svg>");
+		--box--border__top-right: url("data:image/svg+xml,<svg width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'><rect x='-9' y='1' width='18' height='18' rx='6' ry='6' stroke='black' stroke-width='3' /></svg>");
+		--box--border__left: url("data:image/svg+xml,<svg preserveAspectRatio='none' width='10' height='100' viewBox='0 0 10 100' fill='none' xmlns='http://www.w3.org/2000/svg'><line x1='1' y1='-1' x2='1' y2='101' stroke='%23000' stroke-width='3'/></svg>");
+		--box--border__right: url("data:image/svg+xml,<svg preserveAspectRatio='none' width='10' height='100' viewBox='0 0 10 100' fill='none' xmlns='http://www.w3.org/2000/svg'><line x1='9' y1='-1' x2='9' y2='101' stroke='%23000' stroke-width='3'/></svg>");
+		--box--border__bottom-left: url("data:image/svg+xml,<svg width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'><rect x='1' y='-9' width='18' height='18' rx='6' ry='6' stroke='%23000' stroke-width='3' /></svg>");
+		--box--border__bottom: url("data:image/svg+xml,<svg preserveAspectRatio='none' width='100' height='10' viewBox='0 0 100 10' fill='none' xmlns='http://www.w3.org/2000/svg'><line x1='-1' y1='9' x2='101' y2='9' stroke='%23000' stroke-width='3'/></svg>");
+		--box--border__bottom-right: url("data:image/svg+xml,<svg width='10' height='10' viewBox='0 0 10 10' fill='none' xmlns='http://www.w3.org/2000/svg'><rect x='-9' y='-9' width='18' height='18' rx='6' ry='6' stroke='black' stroke-width='3' /></svg>");
+
+		position: relative;
+		overflow: hidden;
+	}
+
+	.gradient-border::before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: var(--box-border--border);
+		mask:
+			var(--box--border__top-left) 0 0 / 0.625rem 0.625rem,
+			var(--box--border__top) 0.625rem 0 / calc(100% - 1.25rem) 0.625rem,
+			var(--box--border__top-right) 100% 0 / 0.625rem 0.625rem,
+			var(--box--border__left) 0 0.625rem / 0.625rem calc(100% - 1.25rem),
+			var(--box--border__right) 100% 0.625rem / 0.625rem calc(100% - 1.25rem),
+			var(--box--border__bottom-left) 0 100% / 0.625rem 0.625rem,
+			var(--box--border__bottom) 0.625rem 100% / calc(100% - 1.25rem) 0.625rem,
+			var(--box--border__bottom-right) 100% 100% / 0.625rem 0.625rem;
+		mask-repeat: no-repeat;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,65 @@
 <script lang="ts">
   import "milligram/dist/milligram.min.css";
   import TMSim from "$lib/TM/TMSim.svelte"
+
+  let codeElement;
+
+  const getCaretPosition = (elementRef) => {
+    var selection = document.getSelection();
+    if (!selection || !elementRef) return 0;
+    selection.collapseToEnd();
+    const range = selection.getRangeAt(0);
+    const clone = range.cloneRange();
+    clone.selectNodeContents(elementRef);
+    clone.setEnd(range.startContainer, range.startOffset);
+    return clone.toString().length;
+  }
+
+  const setCaretPosition = (elem, caretPos) => {
+
+      if(elem != null) {
+          if(elem.createTextRange) {
+              var range = elem.createTextRange();
+              range.move('character', caretPos);
+              range.select();
+          }
+          else {
+              if(elem.selectionStart) {
+                  elem.focus();
+                  elem.setSelectionRange(caretPos, caretPos);
+              }
+              else
+                  elem.focus();
+          }
+      }
+  }
+  
+  function handleKeyDown(event: KeyboardEvent){
+    // console.dir(event);
+    switch(event.key) {
+      case "Tab":
+        {
+          event.preventDefault();
+          const caretPos = getCaretPosition(codeElement);
+          console.dir(codeElement.textContent.valueOf())
+          codeElement.textContent = 
+            codeElement.textContent.substring(0, caretPos)
+            + '\t'
+            + codeElement.textContent.substring(caretPos);
+
+          setTimeout(()=>{
+            setCaretPos(codeElement, caretPos + 1);
+          }, 4)
+        }
+    }
+  }
 </script>
 
 <h1>TuMaSim</h1>
 
 <form>
   <fieldset>
-    <pre><code contenteditable=true></code></pre>
+    <pre><code contenteditable=true on:keydown={handleKeyDown} bind:this={codeElement}></code></pre>
     
     <label for="initialState">Initial state:</label>
     <input type="text" placeholder="A" id="initialState" pattern="\w+">
